b525afa54404c93f81217b8954c3c4fbc55d091d
VitoChien <406618818@qq.com>
2018-10-06 22:27:54 +0800

    fix bugs in roi align

--- src/caffe/layers/roi_align_layer.cu
+++ src/caffe/layers/roi_align_layer.cu
@@ -188,7 +188,7 @@ __global__ void ROIAlignForward(const int nthreads, const Dtype* bottom_data,
         const Dtype x = roi_start_w + pw * bin_size_w +
             static_cast<Dtype>(ix + .5f) * bin_size_w /
                 static_cast<Dtype>(roi_bin_grid_w);
-                Dtype val = bilinear_interpolate(
+        Dtype val = bilinear_interpolate(
             offset_bottom_data, height, width, y, x, index);
         output_val += val;
       }
@@ -224,7 +224,7 @@ __global__ void ROIAlignBackward(const int nthreads, const Dtype* top_diff,
     const int num_rois, const Dtype spatial_scale,
     const int channels, const int height, const int width,
     const int pooled_height, const int pooled_width, const int sampling_ratio, Dtype* bottom_diff,
-    const Dtype* bottom_rois, const Dtype* extended_rois) {
+    const Dtype* bottom_rois) {
     
   CUDA_KERNEL_LOOP(index, nthreads) {
     // (n, c, h, w) coords in bottom data
@@ -238,14 +238,14 @@ __global__ void ROIAlignBackward(const int nthreads, const Dtype* top_diff,
     for (int roi_n = 0; roi_n < num_rois; ++roi_n) {
       // offset_bottom_rois：用来遍历输入ROI的指针      
       const Dtype* offset_bottom_rois = bottom_rois + roi_n * 5;
-      const Dtype* offset_extended_rois = extended_rois + roi_n * 5;      
+      // const Dtype* offset_extended_rois = extended_rois + roi_n * 5;      
       
       int roi_batch_ind = offset_bottom_rois[0];
 
-      Dtype roi_start_w = offset_extended_rois[1] * spatial_scale;  // include
-      Dtype roi_start_h = offset_extended_rois[2] * spatial_scale;
-      Dtype roi_end_w = offset_extended_rois[3] * spatial_scale;
-      Dtype roi_end_h = offset_extended_rois[4] * spatial_scale;
+      Dtype roi_start_w = offset_bottom_rois[1] * spatial_scale;  // include
+      Dtype roi_start_h = offset_bottom_rois[2] * spatial_scale;
+      Dtype roi_end_w = offset_bottom_rois[3] * spatial_scale;
+      Dtype roi_end_h = offset_bottom_rois[4] * spatial_scale;
 
       Dtype roi_width = max(roi_end_w - roi_start_w, (Dtype)1.);
       Dtype roi_height = max(roi_end_h - roi_start_h, (Dtype)1.);
@@ -320,7 +320,6 @@ void ROIAlignLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     return;
   }
   const Dtype* bottom_rois = bottom[1]->gpu_data();
-  const Dtype* extended_rois = bottom[2]->gpu_data();
     
   const Dtype* top_diff = top[0]->gpu_diff();
   Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
@@ -330,7 +329,7 @@ void ROIAlignLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   // NOLINT_NEXT_LINE(whitespace/operators)
   ROIAlignBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
       count, top_diff, top[0]->num(), spatial_scale_, channels_,
-      height_, width_, pooled_height_, pooled_width_, sampling_ratio_, bottom_diff, bottom_rois, extended_rois);
+      height_, width_, pooled_height_, pooled_width_, sampling_ratio_, bottom_diff, bottom_rois);
   CUDA_POST_KERNEL_CHECK;
     
 }
